51bfd57902a54c53f61ba6edde57c07f5bcb15e3
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-13 23:15:36 +0700

    Update Telegram.py

--- AmangMusic/platforms/Telegram.py
+++ AmangMusic/platforms/Telegram.py
@@ -18,7 +18,7 @@ from pyrogram.types import (InlineKeyboardButton,
 
 import config
 from config import MUSIC_BOT_NAME, lyrical
-from CilikMusic import app
+from AmangMusic import app
 
 from ..utils.formatters import (convert_bytes, get_readable_time,
                                 seconds_to_min)
