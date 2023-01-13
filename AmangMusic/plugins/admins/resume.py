748ca9b4af73baef6b9634878391460923560225
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-13 23:23:03 +0700

    Update resume.py

--- AmangMusic/plugins/admins/resume.py
+++ AmangMusic/plugins/admins/resume.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from CilikMusic import app
-from CilikMusic.core.call import Cilik
-from CilikMusic.utils.database import is_music_playing, music_on
-from CilikMusic.utils.decorators import AdminRightsCheck
+from AmangMusic import app
+from AmangMusic.core.call import Amang
+from AmangMusic.utils.database import is_music_playing, music_on
+from AmangMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 RESUME_COMMAND = get_command("RESUME_COMMAND")
@@ -34,7 +34,7 @@ async def resume_com(cli, message: Message, _, chat_id):
     if await is_music_playing(chat_id):
         return await message.reply_text(_["admin_3"])
     await music_on(chat_id)
-    await Cilik.resume_stream(chat_id)
+    await Amang.resume_stream(chat_id)
     await message.reply_text(
         _["admin_4"].format(message.from_user.mention)
     )
