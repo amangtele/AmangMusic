cf3caf4f3b0668c73a7ea06354b8ccf2403abcff
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-13 23:26:14 +0700

    Update unmute.py

--- AmangMusic/plugins/admins/unmute.py
+++ AmangMusic/plugins/admins/unmute.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from CilikMusic import app
-from CilikMusic.core.call import Cilik
-from CilikMusic.utils.database import is_muted, mute_off
-from CilikMusic.utils.decorators import AdminRightsCheck
+from AmangMusic import app
+from AmangMusic.core.call import Amang
+from AmangMusic.utils.database import is_muted, mute_off
+from AmangMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 UNMUTE_COMMAND = get_command("UNMUTE_COMMAND")
@@ -34,7 +34,7 @@ async def unmute_admin(Client, message: Message, _, chat_id):
     if not await is_muted(chat_id):
         return await message.reply_text(_["admin_7"])
     await mute_off(chat_id)
-    await Cilik.unmute_stream(chat_id)
+    await Amang.unmute_stream(chat_id)
     await message.reply_text(
         _["admin_8"].format(message.from_user.mention)
     )
