0bdb03e4c9386ac4fc28646b9752ead5da31d656
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-14 00:09:51 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,7 +57,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/grey423/CilikMusic",
+    "https://github.com/amangtele/AmangMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
@@ -67,10 +67,10 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 # Only  Links formats are  accepted for this Var value.
 SUPPORT_CHANNEL = getenv(
     "SUPPORT_CHANNEL", None
-)  # Example:- https://t.me/CilikProject
+)  # Example:- https://t.me/amwangs
 SUPPORT_GROUP = getenv(
     "SUPPORT_GROUP", None
-)  # Example:- https://t.me/CilikSupport
+)  # Example:- https://t.me/amangsupportgrup
 
 # Set it in True if you want to leave your assistant after a certain amount of time. [Set time via AUTO_LEAVE_ASSISTANT_TIME]
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", None)
@@ -155,7 +155,7 @@ STRING5 = getenv("STRING_SESSION5", None)
 BANNED_USERS = filters.user()
 YTDOWNLOADER = 1
 LOG = 2
-LOG_FILE_NAME = "Ciliklogs.txt"
+LOG_FILE_NAME = "Amanglogs.txt"
 adminlist = {}
 lyrical = {}
 chatstats = {}
