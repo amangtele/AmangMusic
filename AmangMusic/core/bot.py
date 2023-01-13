f07d8cc316b25ddb1416c0e412cbd1358697e03a
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-13 23:07:04 +0700

    Update bot.py

--- AmangMusic/core/bot.py
+++ AmangMusic/core/bot.py
@@ -16,11 +16,11 @@ import config
 from ..logging import LOGGER
 
 
-class CilikBot(Client):
+class AmangBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
         super().__init__(
-            "CilikMusicBot",
+            "AmangMusic",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
