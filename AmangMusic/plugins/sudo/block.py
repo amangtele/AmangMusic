46797adc8bec877b8440507b89838643dc991a3c
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-13 23:43:30 +0700

    Update block.py

--- AmangMusic/plugins/sudo/block.py
+++ AmangMusic/plugins/sudo/block.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from CilikMusic import app
-from CilikMusic.misc import SUDOERS
-from CilikMusic.utils.database import add_gban_user, remove_gban_user
-from CilikMusic.utils.decorators.language import language
+from AmangMusic import app
+from AmangMusic.misc import SUDOERS
+from AmangMusic.utils.database import add_gban_user, remove_gban_user
+from AmangMusic.utils.decorators.language import language
 
 # Command
 BLOCK_COMMAND = get_command("BLOCK_COMMAND")
