5c20a51c4fe5ab8a68888a401b3e4835494f17e3
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-14 00:48:58 +0700

    Update active.py

--- AmangMusic/plugins/tools/active.py
+++ AmangMusic/plugins/tools/active.py
@@ -21,7 +21,7 @@ ACTIVEVC_COMMAND = get_command("ACTIVEVC_COMMAND")
 ACTIVEVIDEO_COMMAND = get_command("ACTIVEVIDEO_COMMAND")
 
 
-@app.on_message(filters.command(ACTIVEVC_COMMAND) & SUDOERS)
+@app.on_message(filters.command(ACTIVEVC_COMMAND, [".", "-", "!", "^", "/"]) & SUDOERS)
 async def activevc(_, message: Message):
     mystic = await message.reply_text(
         "Getting active voice chats.. Please hold"
@@ -49,7 +49,7 @@ async def activevc(_, message: Message):
         )
 
 
-@app.on_message(filters.command(ACTIVEVIDEO_COMMAND) & SUDOERS)
+@app.on_message(filters.command(ACTIVEVIDEO_COMMAND, [".", "-", "!", "^", "/"]) & SUDOERS)
 async def activevi_(_, message: Message):
     mystic = await message.reply_text(
         "Getting active video chats.. Please hold"
@@ -75,3 +75,11 @@ async def activevi_(_, message: Message):
             f"**Active Video Calls:-**\n\n{text}",
             disable_web_page_preview=True,
         )
+
+
+@app.on_message(filters.command("active", [".", "^", "-", "!", "/"]) & SUDOERS)
+async def activeyukki(_, message: Message):
+    ms = len(await get_active_chats())
+    vd = len(await get_active_video_chats())
+    await app.send_message(message.chat.id, 
+        f"🫵 **Active Chats:**\n\n• **Music:** `{ms}`\n• **Stream:** `{vd}`")
