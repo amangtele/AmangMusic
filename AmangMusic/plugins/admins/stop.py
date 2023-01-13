a3857626b8da407025aaf7b86547f316ee73b83f
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-13 23:25:40 +0700

    Update stop.py

--- AmangMusic/plugins/admins/stop.py
+++ AmangMusic/plugins/admins/stop.py
@@ -12,10 +12,10 @@ from pyrogram.types import Message
 
 from config import BANNED_USERS
 from strings import get_command
-from CilikMusic import app
-from CilikMusic.core.call import Cilik
-from CilikMusic.utils.database import set_loop
-from CilikMusic.utils.decorators import AdminRightsCheck
+from AmangMusic import app
+from AmangMusic.core.call import Amang
+from AmangMusic.utils.database import set_loop
+from AmangMusic.utils.decorators import AdminRightsCheck
 
 # Commands
 STOP_COMMAND = get_command("STOP_COMMAND")
@@ -31,7 +31,7 @@ STOP_COMMAND = get_command("STOP_COMMAND")
 async def stop_music(cli, message: Message, _, chat_id):
     if not len(message.command) == 1:
         return await message.reply_text(_["general_2"])
-    await Cilik.stop_stream(chat_id)
+    await Amang.stop_stream(chat_id)
     await set_loop(chat_id, 0)
     await message.reply_text(
         _["admin_9"].format(message.from_user.mention)
