c77483bff6e1f6b485fda4d1b62aa30e531dc145
amangtele <122607345+amangtele@users.noreply.github.com>
2023-01-14 21:04:57 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -16,7 +16,7 @@ from pyrogram import filters
 
 load_dotenv()
 
-MUST_JOIN = int(getenv("MUST_JOIN, t.me/idcryptoofficial"))
+MUST_JOIN = int(getenv("MUST_JOIN," "-1001284445583"))
 
 # Get it from my.telegram.org
 API_ID = int(getenv("API_ID", ""))
