15d737acaee9277b617250e3e914062cbd002add
ᴀᴍᴀɴɢ <122607345+amangtele@users.noreply.github.com>
2023-03-02 11:31:20 +0700

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,7 +57,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/amangtele/AmangMusic",
+    "https://github.com/amanqs/AmangMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
